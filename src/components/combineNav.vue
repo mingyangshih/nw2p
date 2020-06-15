bb11d8b4c52d9eaae4d6c0d65e326f2f6e99e94c
ming <jsps595214@gmail.com>
2020-06-15 15:00:39 +0800

    新增dev分支 關掉SSO

--- src/components/combineNav.vue
+++ src/components/combineNav.vue
@@ -34,7 +34,7 @@
               p(v-else @click.prevent="standard(eachCategoryProduct[idx][0])").font-weight-bold.pl-3.py-2.mb-0.allProdItemDetailItem.fz14.text-decoration.none.text-dark <span @click="sideBarShowEvent">{{item}}</span>
               p.mb-0.font-weight-bold.pl-3.py-2.allProdItemDetailItem.fz14.text-decoration-none.text-dark( v-for="(item1,idx1) in totalProduct" :key="idx1" v-if="item1.productCategory === item" @click.prevent="standard(item1.productId)") - <span >{{item1.productName}}</span>
       label(@click="serviceContent").item.helpCenter.mb-0.align-items-center 幫助中心
-      label.mb-0.ml-3(data-toggle="modal" data-target="#loginModal") SSO登入測試
+      //- label.mb-0.ml-3(data-toggle="modal" data-target="#loginModal") SSO登入測試
       router-link(to="/aboutYFP").text-dark.item.aboutUs.mb-0.align-items-center.text-decoration-none 關於我們
       //- router-link(to="/serviceContent").item.saledHelp.mb-0.align-items-center 售後服務
       label(@click="serviceContent").item.contactUs.mb-0.align-items-center 聯絡我們
