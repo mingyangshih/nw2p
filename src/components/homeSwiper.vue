a759531f54ba901de82f6c256c173df3ed9bcaee
ming <jsps595214@gmail.com>
2020-09-29 16:53:15 +0800

    home swiper

--- src/components/homeSwiper.vue
+++ src/components/homeSwiper.vue
@@ -2,23 +2,29 @@
   <div class="swiperComponent">
     <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop" v-if="viewportWidth > 640">
     <!-- slides -->
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner04.jpg" alt="" class="img-fluid"></swiper-slide>
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner.jpg" alt="" class="img-fluid"></swiper-slide>
+      <swiper-slide class="bigImg" v-for="item in pcBanner" :key="item.displayseq" v-if="Date.parse(item.issueEndDate) > time">
+        <img :src="item.img" alt="" class="img-fluid" v-if="item.enabled === 'Y'" @click="openUrl(item.href)">
+        <img :src="item.img" alt="" class="img-fluid" v-else>
+      </swiper-slide>
+      <!-- <swiper-slide class="bigImg" ><img src="../assets/img/home/banner.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner02.jpg" alt="" class="img-fluid"></swiper-slide>
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner03.jpg" alt="" class="img-fluid"></swiper-slide>
+      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner03.jpg" alt="" class="img-fluid"></swiper-slide> -->
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
     <swiper :options="swiperOptionTop" class="gallery-top" ref="swiperTop" v-if="viewportWidth < 640">
     <!-- slides -->
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile07.jpg" alt="" class="img-fluid"></swiper-slide>
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile08.jpg" alt="" class="img-fluid"></swiper-slide>
+      <swiper-slide class="bigImg" v-for="item in mobildBanner" :key="item.displayseq" v-if="Date.parse(item.issueEndDate) > time">
+        <img :src="item.img" alt="" class="img-fluid" v-if="item.enabled === 'Y'" @click="openUrl(item.href)">
+        <img :src="item.img" alt="" class="img-fluid" v-else>
+      </swiper-slide>
+      <!-- <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile08.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile01.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile02.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile03.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile04.jpg" alt="" class="img-fluid"></swiper-slide>
       <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile05.jpg" alt="" class="img-fluid"></swiper-slide>
-      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile06.jpg" alt="" class="img-fluid"></swiper-slide>
+      <swiper-slide class="bigImg" ><img src="../assets/img/home/banner_mobile06.jpg" alt="" class="img-fluid"></swiper-slide> -->
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
     </swiper>
@@ -36,8 +42,16 @@ export default {
     swiper,
     swiperSlide
   },
+  methods: {
+    openUrl (url) {
+      window.open(url)
+    }
+  },
   data () {
     return {
+      pcBanner: [],
+      mobildBanner: [],
+      time: null,
       swiperOptionTop: {
         spaceBetween: 10,
         slidesPerView: 1,
@@ -56,6 +70,18 @@ export default {
         }
       }
     }
+  },
+  async created () {
+    const vm = this
+    let API_PATH = process.env.API
+    await this.$http.get(`${API_PATH}banner/list`).then(response => {
+      let pcBanner = response.data.data[0].pc
+      let mobildBanner = response.data.data[1].mobile
+      vm.pcBanner = pcBanner
+      vm.mobildBanner = mobildBanner
+    })
+    let time = new Date().getTime()
+    vm.time = time
   }
 }
 </script>
